759a6c204a3bf799c2241965f3808aae7d9dce19
TheNotary <no@mail.plz>
2013-07-06 22:31:26 -0500

    initial commit, about to switch on haml... i think...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ControlSwitch::Application.config.secret_key_base = '4eb6707f98cf44f112a043dc5f860b9724c925ffd5a5d708c5678381d51b4042a649f2bc62f023729fa4f868c1aa9223ab94424d3c1fff5f74cedf232f00d66c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ControlSwitch::Application.config.session_store :cookie_store, key: '_control_switch_session'
