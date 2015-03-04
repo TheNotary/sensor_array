1be7c80378a17770cf56e2bd026c48a6457ab848
TheNotary <no@mail.plz>
2015-03-04 17:57:53 -0600

    created init.d style start stop method

---
+++ docs/control_switch.init.d
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+### BEGIN INIT INFO
+# Provides:          thin
+# Required-Start:    $local_fs $remote_fs $network $syslog
+# Required-Stop:     $local_fs $remote_fs $network $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: starts the thin web server
+# Description:       starts thin
+### END INIT INFO
+
+# Add to auto-start on server start
+# sudo update-rc.d control_switch defaults
+# Remove from auto-start
+# sudo update-rc.d -f control_switch remove
+
+#RVM_ENV="ruby-2.0.0-p195@global"
+RVM_ENV="default"
+APP_NAME="control_switch"
+APP_PATH="/home/pi/dev/rails/$APP_NAME"
+EUSER="pi"
+
+RVM="source /home/pi/.rvm/environments/$RVM_ENV"
+
+CMD="$RVM && cd $APP_PATH && bundle exec thin"
+CMD_OPTS="start -e development -p 3000 -d"
+PID="$APP_PATH/tmp/pids/thin.pid"
+
+NAME=`basename $0`
+DESC="Thin app for $APP_NAME"
+
+case "$1" in
+  start)
+    echo -n "Starting $DESC: "
+    su - $EUSER -c "$CMD $CMD_OPTS"
+    echo "$NAME."
+  ;;
+
+  stop)
+    echo -n "Stopping $DESC: "
+    if [ -f $PID ] && [ -e /proc/$(cat $PID) ]
+    then
+      kill -QUIT `cat $PID`
+      echo "$NAME."
+    else
+      echo "Unable to get $PID file, or process is down"
+    fi
+  ;;
+
+  restart)
+    echo -n "Restarting $DESC: "
+    if [ -f $PID ] && [ -e /proc/$(cat $PID) ]
+    then
+      kill -USR2 `cat $PID`
+    else
+      su - $EUSER -c "$CMD $CMD_OPTS"
+    fi
+    echo "$NAME."
+  ;;
+
+  *)
+    echo "Usage: $NAME {start|stop|restart}" >&2
+    exit 1
+  ;;
+esac
+
+exit 0
