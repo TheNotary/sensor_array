d7b8187008b215d6e9708d21bbdf09d0bed10652
TheNotary <no@mail.plz>
2015-03-10 20:02:55 -0500

    fixed script, added some installation procedure

--- public/scripts/check_pin_state.py
+++ public/scripts/check_pin_state.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 
-from RPi.GPIO import GPIO
+import RPi.GPIO as GPIO
 
 pin_to_check = 11
 
 # the pin numbers refer to the board connector not the chip
-GPIO.setmode(GPIO.BOARD)  
+GPIO.setmode(GPIO.BOARD)
 
 # set up pin  (one of the above listed pins) as an input with a pull-up resistor
-GPIO.setup(pin_to_check, GPIO.INPUT, GPIO.PUD_UP) 
+GPIO.setup(pin_to_check, GPIO.IN, GPIO.PUD_UP)
 
 if GPIO.input(pin_to_check):
-    print "switch is open"
+    print("switch is open")
 else:
-    print "switch is closed"
+    print("switch is closed")
 
