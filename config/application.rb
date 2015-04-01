1320c28e219c20921bf31b2ca8809dab5d1cbb70
TheNotary <no@mail.plz>
2015-03-31 20:12:54 -0500

    making database more optional

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,15 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+if ENV['HEROKU_DEPLOYING'].nil?
+  require 'rails/all'
+else
+  require "action_controller/railtie"
+  require "action_mailer/railtie"
+  require "sprockets/railtie"
+  require "rails/test_unit/railtie"
+end
+
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
